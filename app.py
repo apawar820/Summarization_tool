c62bdb74e67262632c95d6330f0b463e9a7fadab
apawar820 <133425256+apawar820@users.noreply.github.com>
2024-04-25 10:49:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import pymongo
 # Connect to MongoDB Atlas
 def connect_to_mongodb():
     # Replace these values with your MongoDB Atlas connection string
-    username = "your_username"
-    password = "your_password"
-    cluster_name = "your_cluster_name"
-    database_name = "your_database_name"
+    username = "akhileshpawar820"
+    password = "Akhi8011"
+    cluster_name = "Cluster"
+    database_name = "Cluster"
 
     try:
-        client = pymongo.MongoClient(f"mongodb+srv://{username}:{password}@{cluster_name}.mongodb.net/{database_name}?retryWrites=true&w=majority")
+        client = pymongo.MongoClient(f"mongodb+srv://akhileshpawar820:<Akhi8011*>@cluster.2neubhc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster")
         db = client[database_name]
         return db
     except Exception as e:
